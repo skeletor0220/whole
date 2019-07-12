ece81cd80d1b969cc1c0907b36ab4084c5033b9f
Elizabeth A. Fujita <syp001@c28-0113-20.ad.mtu.edu>
2019-07-12 13:38:55 -0400

    all app codes

---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.log("Today is Thursday");
+console.table({a:1, b:2});
+console.table([0,1,2,3]);
+console.error("!");
+console.warn("warn");
+function myfunction1() {
+    var num1 = 5;
+    var num2 = 10;
+    var num3;
+    num3 = num1 + num2;
+    alert(num1 + "+" + num2 + "=" + num3);
+}
+function myfunction2() {
+    var num1 = 5;
+    var num2 = 10;
+    var num3;
+    num3 = num1 - num2;
+    alert(num1 + "-" + num2 + "=" + num3);
+}
+function myfunction3() {
+    var num1 = 5;
+    var num2 = 10;
+    var num3;
+    num3 = num1 * num2;
+    alert(num1 + "*" + num2 + "=" + num3);
+}
+function myfunction4() {
+    var num1 = 5;
+    var num2 = 10;
+    var num3;
+    num3 = num1 / num2;
+    alert(num1 + "/" + num2 + "=" + Math.floor(num3));
+}
+function add() {
+    var num1 = "good ";
+    var num2 = "morning";
+    var num3;
+    num3 = num1 + num2;
+    alert(num1 + "+" + num2 + "=" + num3);
+
+}
+function on() {
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function off() {
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
---
+++ pokemon1/app.js
@@ -0,0 +1,25 @@
+string var1 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/bulbasaur.png";
+string var2 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/charmander.png";
+string var3 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/squirtle.png";
+string var4 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/metapod.png";
+string var5 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/pikachu.png";
+string var6 = "https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/jigglypuff.png";
+
+function bulbasaur() {
+    document.getElementById('pokemon').src = var1;
+}
+function bulbasaur() {
+    document.getElementById('pokemon').src = var2;
+}
+function bulbasaur() {
+    document.getElementById('pokemon').src = var3;
+}
+function bulbasaur() {
+    document.getElementById('pokemon').src = var4;
+}
+function bulbasaur() {
+    document.getElementById('pokemon').src = var5;
+}
+function bulbasaur() {
+    document.getElementById('pokemon').src = var6;
+}
